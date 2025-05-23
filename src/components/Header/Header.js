55129667636d4f2569ad0665ad0a9f32e54f541c
Caio Rocha <rochacaiodev@gmail.com>
2025-05-23 05:40:41 -0300

    feature: implement useSoundEnabled hook on multiple components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,12 @@ import VisuallyHidden from '../VisuallyHidden';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import styles from './Header.module.css';
 
+import { useSoundEnabled } from '../SoundEnabledProvider/SoundEnabledProvider';
+
 function Header() {
   const id = React.useId();
 
-  // TODO: Global state?
-  const soundEnabled = true;
+ const { soundEnabled, setSoundEnabled } = useSoundEnabled()
 
   return (
     <header className={styles.wrapper}>
@@ -21,7 +22,7 @@ function Header() {
 
         <button
           onClick={() => {
-            // TODO: flip `soundEnabled`
+            setSoundEnabled(!soundEnabled)
           }}
         >
           {soundEnabled ? (
